481f542475a8ef5686d8d63dfb9d07ed0718aaf8
Joel Dare <joel@joeldare.com>
2023-05-12 09:03:48 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Lil
+
+A little module of functions I've needed for Go.
---
+++ file_test.go
@@ -0,0 +1,39 @@
+package lil
+
+import (
+	"testing"
+
+	"golang.org/x/exp/slices"
+)
+
+func TestFileToSlice(t *testing.T) {
+	name := "FileToArray()"
+
+	// Read the file into an array
+	repos, err := File("testfile/file.txt")
+	if err != nil {
+		t.Fatalf(`%v: unable to read file.txt: %v`, name, err)
+	}
+
+	// Verify that we got 6 elements
+	wantLen := 6
+	gotLen := len(repos)
+	if gotLen != wantLen {
+		t.Fatalf(`%v: want length of %v got %v`, name, wantLen, gotLen)
+	}
+
+	// Verify that "one" is there
+	want := "one"
+	got := slices.Contains(repos, want)
+	if got != true {
+		t.Fatalf(`%v: want true for %v got %v`, name, want, got)
+	}
+
+	// Verify that "six" is there
+	want = "six"
+	got = slices.Contains(repos, want)
+	if got != true {
+		t.Fatalf(`%v: want true for %v got %v`, name, want, got)
+	}
+
+}
