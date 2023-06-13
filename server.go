881c58f44a944dd02f01b0b0ba3e11ff6f07798f
Joel Dare <joel@joeldare.com>
2023-06-13 07:37:17 -0600

    feat: add GetServerPort() function
    This function returns a string containing the value of the PORT command or returns 8080 if no default port is set.

---
+++ server.go
@@ -0,0 +1,13 @@
+package lil
+
+import "os"
+
+// Return the port to use as a string. Use the PORT environment variable if it
+// exists otherwise default to 8080.
+func GetServerPort() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	return port
+}
---
+++ server_test.go
@@ -0,0 +1,27 @@
+package lil
+
+import (
+	"os"
+	"testing"
+)
+
+func TestGetServerPort(t *testing.T) {
+	name := "GetServerPort()"
+
+	// Verify that we got the default port of 8080 if PORT is not set
+	os.Unsetenv("PORT")
+	wantPort := "8080"
+	gotPort := GetServerPort()
+	if gotPort != wantPort {
+		t.Fatalf(`%v: want port of %v got %v`, name, wantPort, gotPort)
+	}
+
+	// Verify that we got a custom port of 9090 when setting PORT to 9090
+	t.Setenv("PORT", "9090")
+	wantPort = "9090"
+	gotPort = GetServerPort()
+	if gotPort != wantPort {
+		t.Fatalf(`%v: want port of %v got %v`, name, wantPort, gotPort)
+	}
+
+}
